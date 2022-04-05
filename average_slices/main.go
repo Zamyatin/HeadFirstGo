87862f11827b45ca044cee7ff671a58bc1d17e7f
Raj Singh <zamyatin@users.noreply.github.com>
2022-04-05 10:20:28 -0500

    it works!

--- average_slices/main.go
+++ average_slices/main.go
@@ -2,10 +2,22 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/zamyatin/go-averages/datafile"
 )
 
 func main() {
-	fmt.Println(datafile.GetFloats("data.txt"))
+	numbers, err := datafile.GetFloats("data.txt")
+	if err != nil {
+		log.Fatal(numbers, err)
+	}
+
+	var sum float64 = 0
+	for _, number := range numbers {
+		sum += number
+	}
+
+	sampleCount := float64(len(numbers))
+	fmt.Printf("Average: %0.2f\n", sum/sampleCount)
 }
