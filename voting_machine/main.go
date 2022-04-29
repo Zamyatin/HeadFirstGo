6983921b6a1dbc0e9cadca0bd5a462e84a88554a
Raj Singh <zamyatin@users.noreply.github.com>
2022-04-29 11:32:02 -0500

    voting machine works!

--- voting_machine/main.go
+++ voting_machine/main.go
@@ -1,9 +1,26 @@
 package main
 
 import (
-	"github.com/Zamyatin/HeadFirstGo/average_slices/datafile"
+	"fmt"
+	"log"
+
+	"github.com/zamyatin/HeadFirstGo/average_slices/datafile"
 )
 
 func main() {
+	votes, err := datafile.GetStrings("voteData.txt")
 
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var voteCount = make(map[string]int)
+
+	for _, name := range votes {
+		voteCount[name]++
+	}
+
+	for k, v := range voteCount {
+		fmt.Printf("%s: %d\n", k, v)
+	}
 }
